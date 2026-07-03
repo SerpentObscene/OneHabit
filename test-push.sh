cabed218e49678d3add104715215bd01d5c2f1b1
SerpentObscene <fredrik.fridlund@regionvarmland.se>
2026-07-03 23:20:50 +0200

    Update test script with cron secret header

--- test-push.sh
+++ test-push.sh
@@ -3,4 +3,5 @@ curl -s -X POST \
   "https://ccgymqxngnzqpxttdisd.supabase.co/functions/v1/send-reminders" \
   -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjZ3ltcXhuZ256cXB4dHRkaXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNjk1NzIsImV4cCI6MjA5ODY0NTU3Mn0.yNzc1L_NsyMXG5WXsuxS2oghOult5HMBUCNVMS3IKfI" \
   -H "Content-Type: application/json" \
+  -H "x-cron-secret: onehabit-cron-2026" \
   -d '{}'
